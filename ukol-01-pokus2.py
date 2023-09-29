64173c8066637472bd743ffff7f39d4ed7c2f1a0
Gabriela Skoupa <gabriela.skoupa@seznam.cz>
2023-09-29 15:12:17 +0200

    doplneny nepovinne casti (ne vsechny)

--- ukol-01-pokus2.py
+++ ukol-01-pokus2.py
@@ -1,3 +1,10 @@
 jmeno = "gabriela.skoupa"
 mail = jmeno + "@czechitas.cz"
 print (mail)
+
+krestni = input("Napiště prosím své křestní jméno bez diakritiky: ")
+prijmeni = (input("Napiště prosím své příjmení bez diakritiky: "))
+jmeno_a_prijmeni = krestni +  " " + prijmeni
+print (jmeno_a_prijmeni.upper())
+print (jmeno_a_prijmeni.lower())
+print (krestni[0].upper() + krestni[1:].lower() + " " + prijmeni[0].upper() + prijmeni[1:].lower())
