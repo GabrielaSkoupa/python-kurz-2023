d8683b3e6b9d73ddf83e080cc16b7f00ec228188
Gabriela Skoupa <gabriela.skoupa@seznam.cz>
2023-10-28 09:29:03 +0200

    ctvrty ukol s chybami nevim kde

--- ukol_4.py
+++ ukol_4.py
@@ -1,23 +1,30 @@
-"""První funkce ověří telefonní číslo.
- Uvažuj, že telefonní číslo může být devítimístné nebo třináctimístné (pokud je na začátku předvolba "+420"). 
-Funkce ověří, jestli je číslo platné. Pokud je platné, vrátí hodnotu True, jinak vrátí hodnotu False."""
-
 def format_cisla():
     cislo = input("zadejte telefonní číslo příjemce: ")
-    if len(cislo):9
     delka_cisla = True
-
-    elif len(cislo):13 
-    delka13 = True
-    elif cislo[:3]:"+420"
-    predvolba = True
-        if delka13 and predvolba:
+    if len(cislo)==9:
+        delka_cisla = True
+    elif len(cislo)==13 and cislo[:4]=="+420":
         delka_cisla = True
     else:
         delka_cisla = False 
-    print(delka_cisla)
+    print("neprávný formát čísla")
 
+    return delka_cisla
 
-    
-format_cisla()
+def cena_zpravy():
+    if delka_cisla == True:
+        text = input("zadejte text zprávy: ")
+
+    if len(text)<=180:
+        print ("Cena vaší zprávy je 3 Kč.")
+    elif len(text)>=181 and len(text)<= 360:
+        print("Cena vaší zprávy je 6 Kč.")
+    elif len(text)>=361 and len(text)<= 540:
+        print("Cena vaší zprávy je 9 Kč.")
+    else:
+        print("vaše zpráva je moc dlouhá")
+
+format_cisla() 
+if delka_cisla == True:
+    cena_zpravy()
 
